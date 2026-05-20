093a6893a56e279704e4e9724ad0603ceb7aadbb
chaykinks <chaykinks@gmail.com>
2026-05-20 20:34:11 +0500

    добавил селекторы для quests slice

---
+++ src/store/quests-slice/selectors.ts
@@ -0,0 +1,29 @@
+import {RootState} from '..';
+import {QuestLevel, QuestType} from '../../const';
+import {Quest} from '../../types/quest';
+
+export const getQuests = (state: RootState): Quest[] => state.QUESTS.quests;
+
+export const getQuestsRequestStatus = (state: RootState) => state.QUESTS.questsRequestStatus;
+
+export const getQuestsError = (state: RootState) => state.QUESTS.questsError;
+
+export const getActiveType = (state: RootState) => state.QUESTS.activeType;
+
+export const getActiveLevel = (state: RootState) => state.QUESTS.activeLevel;
+
+export const getFilteredQuests = (state: RootState): Quest[] => {
+  const quests = getQuests(state);
+  const activeType = getActiveType(state);
+  const activeLevel = getActiveLevel(state);
+
+  return quests.filter((quest) => {
+    const isTypeMatch =
+      activeType === QuestType.All || quest.type === activeType;
+
+    const isLevelMatch =
+      activeLevel === QuestLevel.Any || quest.level === activeLevel;
+
+    return isTypeMatch && isLevelMatch;
+  });
+};
