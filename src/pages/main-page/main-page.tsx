7336ca2c740eeee7d8527fee9c703eb488b47fd7
chaykinks <chaykinks@gmail.com>
2026-05-20 19:57:32 +0500

    добавил разметку компонента главной

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,6 +1,6 @@
-import {RequestStatus} from '../../const';
 import {useSelector} from 'react-redux';
 import {RootState} from '../../store';
+import {RequestStatus} from '../../const';
 
 function MainPage(): JSX.Element {
   const quests = useSelector((state: RootState) => state.QUESTS.quests);
@@ -16,16 +16,83 @@ function MainPage(): JSX.Element {
   }
 
   return (
-    <main>
-      <h1>Каталог квестов</h1>
+    <main className="page-content">
+      <div className="container">
+        <div className="page-content__title-wrapper">
+          <h1 className="subtitle page-content__subtitle">
+            квесты в Санкт-Петербурге
+          </h1>
 
-      <ul>
-        {quests.map((quest) => (
-          <li key={quest.id}>
-            {quest.title}
-          </li>
-        ))}
-      </ul>
+          <h2 className="title title--size-m page-content__title">
+            Выберите тематику
+          </h2>
+        </div>
+
+        <div className="page-content__item">
+          <form className="filter" action="#" method="get">
+            <fieldset className="filter__section">
+              <legend className="visually-hidden">Тематика</legend>
+
+              <ul className="filter__list">
+                <li className="filter__item">
+                  <input type="radio" name="type" id="all" defaultChecked />
+
+                  <label className="filter__label" htmlFor="all">
+                    <svg className="filter__icon" width="26" height="30" aria-hidden="true">
+                      <use xlinkHref="#icon-all-quests"></use>
+                    </svg>
+
+                    <span className="filter__label-text">Все квесты</span>
+                  </label>
+                </li>
+
+                <li className="filter__item">
+                  <input type="radio" name="type" id="adventure" />
+
+                  <label className="filter__label" htmlFor="adventure">
+                    <svg className="filter__icon" width="36" height="30" aria-hidden="true">
+                      <use xlinkHref="#icon-adventure"></use>
+                    </svg>
+
+                    <span className="filter__label-text">Приключения</span>
+                  </label>
+                </li>
+              </ul>
+            </fieldset>
+          </form>
+        </div>
+
+        <h2 className="title visually-hidden">Выберите квест</h2>
+
+        <div className="cards-grid">
+          {quests.map((quest) => (
+            <div className="quest-card" key={quest.id}>
+              <div className="quest-card__img">
+                <img
+                  src={quest.previewImg}
+                  alt={quest.title}
+                />
+              </div>
+
+              <div className="quest-card__content">
+                <div className="quest-card__info-wrapper">
+                  <div className="quest-card__type">
+                    {quest.type}
+                  </div>
+
+                  <div className="quest-card__level">
+                    {quest.level}
+                  </div>
+                </div>
+
+                <h3 className="quest-card__title">
+                  {quest.title}
+                </h3>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
