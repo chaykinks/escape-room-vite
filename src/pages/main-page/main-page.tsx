35dc3cb02eb0f2997947d89f9aa3b0158f03f055
chaykinks <chaykinks@gmail.com>
2026-05-20 18:48:21 +0500

    добавил вывод квестов на главной

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,7 +1,31 @@
+import {RequestStatus} from '../../const';
+import {useSelector} from 'react-redux';
+import {RootState} from '../../store';
+
 function MainPage(): JSX.Element {
+  const quests = useSelector((state: RootState) => state.QUESTS.quests);
+  const questsRequestStatus = useSelector((state: RootState) => state.QUESTS.questsRequestStatus);
+  const questsError = useSelector((state: RootState) => state.QUESTS.questsError);
+
+  if (questsRequestStatus === RequestStatus.Loading) {
+    return <main><p>Loading...</p></main>;
+  }
+
+  if (questsRequestStatus === RequestStatus.Failed) {
+    return <main><p>{questsError}</p></main>;
+  }
+
   return (
     <main>
       <h1>Каталог квестов</h1>
+
+      <ul>
+        {quests.map((quest) => (
+          <li key={quest.id}>
+            {quest.title}
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
