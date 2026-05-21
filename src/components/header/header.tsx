ef0415f281eee82253291fc915f00a085159bb0c
chaykinks <chaykinks@gmail.com>
2026-05-21 19:57:34 +0500

    добавил реализации хедера для залогиненного, разлогиненного и страницы логина

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import {Link, NavLink} from 'react-router-dom';
+import {Link, NavLink, useLocation} from 'react-router-dom';
 import {AppRoute, AuthorizationStatus} from '../../const';
 import {useAppDispatch} from '../../hooks';
 import {logout} from '../../store/user-slice/user-slice';
@@ -9,32 +9,92 @@ type HeaderProps = {
 
 function Header({authorizationStatus}: HeaderProps): JSX.Element {
   const dispatch = useAppDispatch();
+  const {pathname} = useLocation();
   const isAuth = authorizationStatus === AuthorizationStatus.Auth;
+  const isLoginPage = pathname === AppRoute.Login;
 
   const handleLogoutClick = () => {
-    dispatch(logout());
+    void dispatch(logout());
   };
 
+  const getLinkClassName = ({isActive}: {isActive: boolean}) =>
+    isActive ? 'link active' : 'link';
+
   return (
-    <header>
-      <nav>
-        <Link to={AppRoute.Root}>Escape Room</Link>
+    <header className="header">
+      <div className="container container--size-l">
+        <Link
+          className="logo header__logo"
+          to={AppRoute.Root}
+          aria-label="Перейти на Главную"
+        >
+          <svg width="134" height="52" aria-hidden="true">
+            <use xlinkHref="#logo" />
+          </svg>
+        </Link>
 
-        <NavLink to={AppRoute.Root}>Квесты</NavLink>
-        <NavLink to={AppRoute.Contacts}>Контакты</NavLink>
+        <nav className="main-nav header__main-nav">
+          <ul className="main-nav__list">
+            <li className="main-nav__item">
+              <NavLink
+                className={getLinkClassName}
+                to={AppRoute.Root}
+                end
+              >
+                Квесты
+              </NavLink>
+            </li>
 
-        {isAuth && (
-          <NavLink to={AppRoute.MyQuests}>Мои бронирования</NavLink>
-        )}
+            <li className="main-nav__item">
+              <NavLink
+                className={getLinkClassName}
+                to={AppRoute.Contacts}
+              >
+                Контакты
+              </NavLink>
+            </li>
 
-        {isAuth ? (
-          <button type="button" onClick={handleLogoutClick}>
-            Выйти
-          </button>
-        ) : (
-          <NavLink to={AppRoute.Login}>Войти</NavLink>
-        )}
-      </nav>
+            {isAuth && !isLoginPage && (
+              <li className="main-nav__item">
+                <NavLink
+                  className={getLinkClassName}
+                  to={AppRoute.MyQuests}
+                >
+                  Мои бронирования
+                </NavLink>
+              </li>
+            )}
+          </ul>
+        </nav>
+
+        <div className="header__side-nav">
+          {!isLoginPage && (
+            isAuth ? (
+              <button
+                className="btn btn--accent header__side-item"
+                type="button"
+                onClick={handleLogoutClick}
+              >
+                Выйти
+              </button>
+            ) : (
+              <NavLink
+                className="btn btn--accent header__side-item"
+                to={AppRoute.Login}
+              >
+                Войти
+              </NavLink>
+            )
+          )}
+
+          <a
+            className="link header__side-item header__phone-link"
+            href="tel:88001111111"
+          >
+            8 (000) 111-11-11
+          </a>
+        </div>
+      </div>
     </header>
   );
 }
