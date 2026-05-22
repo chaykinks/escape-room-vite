0a52cde1dd0391454ddf4d66a50ea953239d2704
chaykinks <chaykinks@gmail.com>
2026-05-22 19:29:56 +0500

    добавил селекторы для reservations-slice

---
+++ src/store/reservations-slice/selectors.ts
@@ -0,0 +1,7 @@
+import {RootState} from '..';
+
+export const getReservations = (state: RootState) => state.RESERVATIONS.reservations;
+export const getReservationsRequestStatus = (state: RootState) => state.RESERVATIONS.reservationsRequestStatus;
+export const getReservationsError = (state: RootState) => state.RESERVATIONS.reservationsError;
+export const getDeleteReservationRequestStatus = (state: RootState) => state.RESERVATIONS.deleteReservationRequestStatus;
+export const getDeleteReservationError = (state: RootState) => state.RESERVATIONS.deleteReservationError;
