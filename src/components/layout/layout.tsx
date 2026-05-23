75e2be22fde0beea3963e679df95ec7f66b34e00
chaykinks <chaykinks@gmail.com>
2026-05-23 13:40:17 +0500

    добавил компонент футера в лэйаут

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,5 +1,6 @@
 import {Outlet} from 'react-router-dom';
 import Header from '../header/header';
+import Footer from '../footer/footer';
 import {useAppSelector} from '../../hooks';
 
 function Layout(): JSX.Element {
@@ -9,6 +10,7 @@ function Layout(): JSX.Element {
     <>
       <Header authorizationStatus={authorizationStatus} />
       <Outlet />
+      <Footer />
     </>
   );
 }
