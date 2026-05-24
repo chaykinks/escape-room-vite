35f0d534db684314addad1f096fe9fefad1619ba
chaykinks <chaykinks@gmail.com>
2026-05-24 22:44:36 +0500

    добавил иконки для типов квестов

--- src/components/quest-type-filter/quest-type-filter.tsx
+++ src/components/quest-type-filter/quest-type-filter.tsx
@@ -1,7 +1,16 @@
-import {QUEST_TYPE_NAME, QUEST_TYPES} from '../../const';
+import {QUEST_TYPE_NAME, QUEST_TYPES, QuestType} from '../../const';
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {changeType} from '../../store/quests-slice/quests-slice';
 import {getActiveType} from '../../store/quests-slice/selectors';
+
+const QUEST_TYPE_ICONS: Record<QuestType, string> = {
+  all: 'icon-all-quests',
+  adventures: 'icon-adventure',
+  horror: 'icon-horror',
+  mystic: 'icon-mystic',
+  detective: 'icon-detective',
+  'sci-fi': 'icon-sci-fi',
+};
 
 function QuestTypeFilter(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -28,6 +37,15 @@ function QuestTypeFilter(): JSX.Element {
               className="filter__label"
               htmlFor={type}
             >
+              <svg
+                className="filter__icon"
+                width="26"
+                height="30"
+                aria-hidden="true"
+              >
+                <use xlinkHref={`#${QUEST_TYPE_ICONS[type]}`} />
+              </svg>
+
               <span className="filter__label-text">
                 {QUEST_TYPE_NAME[type]}
               </span>
